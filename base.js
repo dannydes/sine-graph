d99af779b58e7eac5ee1e48ca7577ef735eb5a5d
Daniel <desiradaniel2007@hotmail.com>
2014-11-03 16:47:21 +0100

    add vertical line

--- base.js
+++ base.js
@@ -4,13 +4,22 @@ var RADIUS = 100;
 var DIAMETER = RADIUS * 2;
 
 var canvas = document.getElementById('graph'),
-	context = canvas.getContext('2d'),
+	context = canvas.getContext('2d');
 
-	clockwise = true;
+context.beginPath();
+context.moveTo(0, 0);
+context.lineTo(0, canvas.height);
+context.closePath();
+context.stroke();
+
+var clockwise = true;
+
+context.beginPath();
 
 for (var i = 0; i < canvas.width; i += DIAMETER) {
 	context.arc(i+RADIUS, canvas.height/2, RADIUS, 0, Math.PI, clockwise);
 	clockwise = ! clockwise;
 }
 
+context.closePath();
 context.stroke();
