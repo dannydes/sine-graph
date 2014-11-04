2cea28b2df9bcda4afd20c88f8f53a763fa78305
Daniel <desiradaniel2007@hotmail.com>
2014-11-04 22:29:56 +0100

    Minify JavaScript

--- base.js
+++ base.js
@@ -1,56 +1,60 @@
-'use strict';
+(function () {
 
-var RADIUS = 100;
-var DIAMETER = RADIUS * 2;
+	'use strict';
 
-var canvas = document.getElementById('graph'),
-	context = canvas.getContext('2d');
+	var RADIUS = 100;
+	var DIAMETER = RADIUS * 2;
 
-context.beginPath();
-context.moveTo(0, 0);
-context.lineTo(0, canvas.height);
-context.closePath();
-context.stroke();
+	var canvas = document.getElementById('graph'),
+		context = canvas.getContext('2d');
 
-context.beginPath();
-context.moveTo(1, 0);
-context.lineTo(3, 0);
-context.closePath();
-context.stroke();
-
-context.beginPath();
-context.moveTo(1, canvas.height);
-context.lineTo(3, canvas.height);
-context.closePath();
-context.stroke();
-
-context.fillText(' '+1, 4, 7);
-context.fillText(-1, 4, canvas.height);
-context.fillText(' '+0, 4, canvas.height/2)
-
-for (var i = RADIUS, angle = 1/2; i < canvas.width; i += RADIUS, angle += 1/2) {
 	context.beginPath();
-	context.moveTo(i, canvas.height/2+1);
-	context.lineTo(i, canvas.height/2+3);
+	context.moveTo(0, 0);
+	context.lineTo(0, canvas.height);
 	context.closePath();
 	context.stroke();
 
-	var denominator = (angle - Math.floor(angle) === 0.5),
-		numerator = (denominator ? angle*2 : angle),
+	context.beginPath();
+	context.moveTo(1, 0);
+	context.lineTo(3, 0);
+	context.closePath();
+	context.stroke();
 
-		piStr = (numerator !== 1 && numerator ? numerator : '') + '\u03C0' + (denominator ? '/2' : '');
+	context.beginPath();
+	context.moveTo(1, canvas.height);
+	context.lineTo(3, canvas.height);
+	context.closePath();
+	context.stroke();
 
-	context.fillText(piStr, i+2, canvas.height/2+9);
-}
+	context.fillText(' '+1, 4, 7);
+	context.fillText(-1, 4, canvas.height);
+	context.fillText(' '+0, 4, canvas.height/2)
 
-var clockwise = true;
+	for (var i = RADIUS, angle = 1/2; i < canvas.width; i += RADIUS, angle += 1/2) {
+		context.beginPath();
+		context.moveTo(i, canvas.height/2+1);
+		context.lineTo(i, canvas.height/2+3);
+		context.closePath();
+		context.stroke();
 
-context.beginPath();
+		var denominator = (angle - Math.floor(angle) === 0.5),
+			numerator = (denominator ? angle*2 : angle),
 
-for (var i = 0; i < canvas.width; i += DIAMETER) {
-	context.arc(i+RADIUS, canvas.height/2, RADIUS, 0, Math.PI, clockwise);
-	clockwise = ! clockwise;
-}
+			piStr = (numerator !== 1 && numerator ? numerator : '') + '\u03C0' + (denominator ? '/2' : '');
 
-context.closePath();
-context.stroke();
+		context.fillText(piStr, i+2, canvas.height/2+9);
+	}
+
+	var clockwise = true;
+
+	context.beginPath();
+
+	for (var i = 0; i < canvas.width; i += DIAMETER) {
+		context.arc(i+RADIUS, canvas.height/2, RADIUS, 0, Math.PI, clockwise);
+		clockwise = ! clockwise;
+	}
+
+	context.closePath();
+	context.stroke();
+
+})();
